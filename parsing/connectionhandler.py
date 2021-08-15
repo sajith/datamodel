5f4b99d0aefbdb76a1027e213797ce95fae2c820
Yufeng Xin <yxin@renci.org>
2021-08-14 23:30:25 -0400

    connection_handler unittest

--- parsing/connectionhandler.py
+++ parsing/connectionhandler.py
@@ -23,17 +23,21 @@ class ConnectionHandler():
         try:
             id = data['id']
             name=data['name']
-            ingress_port=data['ingress_port']
-            egress_port=data['elf._egress_port']
-            quantity=data['quantity']
-            start_time=data['start_time']
-            end_time=data['end_time']
+            start=data['start_time']
+            end=data['end_time']
+            ingress=data['ingress_port']
+            egress=data['egress_port']
         except KeyError as e:
             raise MissingAttributeException(e.args[0],e.args[0])
 
-        connection=Connection(id=id, name=name, domain_service = service, version=version, time_stamp=time_stamp, nodes=nodes, links=links)
+        connection=Connection(id=id, name=name, start_time = start, end_time = end, ingress_port = ingress, egress_port = egress)
 
         return connection
+    
+    def import_connection(self,file):
+        with open(file, 'r', encoding='utf-8') as data_file:
+            data = json.load(data_file)
+            self.connection = self.import_connection_data(data)
 
     def get_connection():
         return self.connection
